8548e92bdb9f180c97b708e67dc4e8e14fdbac4b
Nepokornyi <vadymkin@gmail.com>
2023-03-28 22:00:03 +0200

    Liquid Transition

---
+++ projects/42 Day - Liquid Transition/main.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    new hoverEffect({
+        parent: document.querySelector('.distortion'),
+        intensity: 0.2,
+        image1: './bw1.jpg',
+        image2: './bw2.jpg',
+        displacementImage: './diss.png'
+    });
+}
