30822097aa0e063b975f516bb57daf6d52982c01
Nepokornyi <vadymkin@gmail.com>
2023-04-03 21:01:39 +0200

    Resize textarea

---
+++ projects/50 Day - Resize Textarea/main.js
@@ -0,0 +1,7 @@
+const textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keyup', (e) => {
+    textarea.style.height = 'auto';
+    let height = e.target.scrollHeight;
+    textarea.style.height = `${height}px`;
+})
