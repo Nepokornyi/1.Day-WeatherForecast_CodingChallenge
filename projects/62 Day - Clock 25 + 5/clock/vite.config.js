fdcf6df076e21b36f21c495e3488f24e6355cb33
Nepokornyi <vadymkin@gmail.com>
2023-04-17 22:11:50 +0200

    editing config files

--- projects/62 Day - Clock 25 + 5/clock/vite.config.js
+++ projects/62 Day - Clock 25 + 5/clock/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FreeCodeCamp-Certification/Clock25/',
+  build: {
+    outDir: '../Clock25',
+  }
 })
